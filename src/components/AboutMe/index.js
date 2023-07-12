5640974008e66aba0d571feec22d830427293495
Griffin Pelletier <umbral.lights@live.com>
2023-07-12 02:01:48 -0400

    Finished Footer links

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,15 +1,19 @@
 import React from "react";
+import recentPhoto from "../../images/recent-picture.jpg";
+import "./aboutMe.css";
 
 function AboutMe() {
   return (
-    <div>
+    <div class="aboutMe">
       <h2>About Me</h2>
+      <img src={recentPhoto} alt="Picture of Griffin" />
       <p>
         My name is Griffin Pelletier. I'm an up and coming full stack web
-        developer with an intermediate knowledge or JavascSctipt. I'm also
-        versed in Mongoose, Express, React, and Node. I'm exited too continue to
-        explore the world of coding, please feel free to take a look at some of
-        my work.
+        developer with an intermediate knowledge or JavascScript. I'm also
+        versed in SQL, Mongoose, Express, React, Node and Graphsql. I've spent
+        many years working in various fields and am looking to make a home in
+        IT. I'm exited too continue to explore the world of coding, please feel
+        free to take a look at some of my work.
       </p>
     </div>
   );
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,31 @@
 import React from "react";
+import "./footer.css";
 
 function Footer() {
   return (
-    <div>
-      <h2>Footer!</h2>
+    <div class="footer">
+      <h2>Find me at:</h2>
+      <a
+        href="https://github.com/Sunderfire"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Github
+      </a>
+      <a
+        href="https://www.linkedin.com/in/griffin-pelletier-a94186283/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        LinkedIn
+      </a>
+      <a
+        href="https://stackoverflow.com/users/22213455/griffin-pelletier"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Stack Overflow
+      </a>
     </div>
   );
 }
